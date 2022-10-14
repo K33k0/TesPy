1c5770248ec5f2ca3481777476c9b573dfd071fd
Kieran Wynne <keewynne90@gmail.com>
2022-10-14 12:48:39 +0100

    Fix deprecation warning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 import toml
 
 # Read the config file
@@ -8,7 +9,9 @@ config = toml.load('./config.toml')
 WEBDRIVER_PATH = config['File_Path']['web_driver']
 SUBDOMAIN = config['Tesseract']['instance']
 
+if __name__ == '__main__':
+    # Start the browser
+    service = Service(WEBDRIVER_PATH)
+    driver = webdriver.Edge(service=service)
+    driver.get(f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_Login/aspx/Login_Launch.aspx')
 
-# Start the browser
-driver = webdriver.Edge(WEBDRIVER_PATH)
-driver.get(f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_Login/aspx/Login_Launch.aspx')
