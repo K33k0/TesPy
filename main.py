62a86135f3bce1cf4c9c601e226605f49b8b1bf8
Kieran Wynne <keewynne90@gmail.com>
2022-10-14 14:11:02 +0100

    Changes parameter name in login function

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ USERNAME = config['Tesseract']['username']
 PASSWORD = config['Tesseract']['password']
 
 
-def login(driver=None):
-    driver.get(f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_Login/aspx/Login_Launch.aspx')
-    driver.find_element(By.ID, "btnsubmit").click()
-    if 'tesseractidp.eu.auth0.com' in driver.current_url:
-        wait = WebDriverWait(driver, 15, poll_frequency=0.5)
+def login(web_driver=None):
+    web_driver.get(f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_Login/aspx/Login_Launch.aspx')
+    web_driver.find_element(By.ID, "btnsubmit").click()
+    if 'tesseractidp.eu.auth0.com' in web_driver.current_url:
+        wait = WebDriverWait(web_driver, 15, poll_frequency=0.5)
         email_input = wait.until(EC.element_to_be_clickable((By.NAME, "email")))
         email_input.send_keys(USERNAME)
         password_input = wait.until(EC.element_to_be_clickable((By.NAME, "password")))
@@ -27,10 +27,11 @@ def login(driver=None):
         submit_button = wait.until(EC.element_to_be_clickable((By.NAME, "submit")))
         submit_button.click()
         wait.until(EC.element_to_be_clickable((By.ID, "scmaster_sidebar")))
-    if f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_MyHome/aspx/myhome.aspx' in driver.current_url:
+    if f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_MyHome/aspx/myhome.aspx' in web_driver.current_url:
         return True
     else:
-        raise Exception(f'Failed to find home: {driver.current_url}')
+        raise Exception(f'Failed to find home: {web_driver.current_url}')
+
 
 if __name__ == '__main__':
     # Start the browser
