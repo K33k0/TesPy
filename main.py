709e717465879acdf2054c2e00a1bd2b31d7c56e
Kieran Wynne <keewynne90@gmail.com>
2022-10-14 12:57:19 +0100

    Implements automated login

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import selenium.webdriver.support.expected_conditions as EC
 import toml
 
 # Read the config file
@@ -8,10 +11,30 @@ config = toml.load('./config.toml')
 # Place constant variables directly below
 WEBDRIVER_PATH = config['File_Path']['web_driver']
 SUBDOMAIN = config['Tesseract']['instance']
+USERNAME = config['Tesseract']['username']
+PASSWORD = config['Tesseract']['password']
+
+
+def login(driver=None):
+    driver.get(f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_Login/aspx/Login_Launch.aspx')
+    driver.find_element(By.ID, "btnsubmit").click()
+    if 'tesseractidp.eu.auth0.com' in driver.current_url:
+        wait = WebDriverWait(driver, 15, poll_frequency=0.5)
+        email_input = wait.until(EC.element_to_be_clickable((By.NAME, "email")))
+        email_input.send_keys(USERNAME)
+        password_input = wait.until(EC.element_to_be_clickable((By.NAME, "password")))
+        password_input.send_keys(PASSWORD)
+        submit_button = wait.until(EC.element_to_be_clickable((By.NAME, "submit")))
+        submit_button.click()
+        wait.until(EC.element_to_be_clickable((By.ID, "scmaster_sidebar")))
+    if f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_MyHome/aspx/myhome.aspx' in driver.current_url:
+        return True
+    else:
+        raise Exception(f'Failed to find home: {driver.current_url}')
 
 if __name__ == '__main__':
     # Start the browser
     service = Service(WEBDRIVER_PATH)
     driver = webdriver.Edge(service=service)
     driver.get(f'https://{SUBDOMAIN}.asolvi.io/ServiceCentre/SC_Login/aspx/Login_Launch.aspx')
-
+    login(driver)
